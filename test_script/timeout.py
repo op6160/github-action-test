c6f8332198fcabbe63a262df0ddf975becf0aa2a
jeong <jeong>
2025-12-16 14:27:30 +0900

    timeout test

--- test_script/timeout.py
+++ test_script/timeout.py
@@ -4,13 +4,12 @@ def printer(msg):
     print(f"[timeout.py] [log]: {msg} {time.strftime('%H:%M:%S')}")
 
 def main():
-    start = time.time()
     cnt = 0
     while(cnt != 1000):
         cnt += 1
         time.sleep(1)
         printer(cnt)
-    printer("good")
+    printer("Done.")
 
 if __name__ == "__main__":
     main()
