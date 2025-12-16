4187d0f74a91f4207738eaf571d90a872eb6754e
jeong <jeong@gafs.co.jp>
2025-12-16 10:43:14 +0900

    add: add new test files(pushing automatically)

---
+++ test_script/push_script.py
@@ -0,0 +1,22 @@
+import os
+
+def main():
+    result_dir = "../result/push_test/"
+    result_file_name = "test_result"
+    result_count = 0
+    result_file_extension = ".txt"
+
+    if not os.path.exists(result_dir):
+        os.mkdir("../result")
+        os.mkdir(result_dir)
+
+    while 1:
+        result_file_path = result_dir+result_file_name+str(result_count)+result_file_extension
+        if not os.path.exists(result_file_path):
+            with open(result_file_path, "w") as file:
+                file.write(str(result_count))
+            return result_count
+        result_count += 1
+
+if __name__ == "__main__":
+    main()
